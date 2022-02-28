bd57cba7b80e2794b51a87e60188a6645aced6a7
Jhon0718 <91797154+Jhon0718@users.noreply.github.com>
2022-02-28 11:32:52 -0600

    Subo archivo Proyecto Bootcamp en la carpeta de back contiene la Api con la que probe el funcionamiento de la base y el script de la base en el proyecto C# correspondiente

---
+++ Backend/Proyecto Bootcamp/Brive.Bootcamp.Proyecto.API/Models/Usuarios.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Brive.Bootcamp.ProyectoAPI.Models
+{
+    [Table("USUARIOS")]
+    public class Usuarios
+    {
+        [Key]
+        public string Email { get; set; }
+
+        [Column("NOMBRE")]
+        public string Nombre { get; set; }
+
+        [Column("APELLIDOS")]
+        public string Apellidos { get; set; }
+
+        [Column("PASSWORD")]
+        public string Password { get; set; }
+
+        [Column("FECHANACIMIENTO")]
+        public DateTime FechaNacimiento { get; set; }
+
+        
+
+    }
+}
---
+++ Backend/Proyecto Bootcamp/Brive.Bootcamp.Proyecto.API/Repositorios/Implementacion/ImpUsuarioRepo.cs
@@ -0,0 +1,30 @@
+﻿using Brive.Bootcamp.ProyectoAPI.DataAccess;
+using Brive.Bootcamp.ProyectoAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Brive.Bootcamp.ProyectoAPI.Repositorios.Implementacion
+{
+    public class ImpUsuarioRepo : IUsuarioRepo
+    {
+        private readonly ProjectContext _contextBD;
+
+        public ImpUsuarioRepo(ProjectContext contextDB)
+        {
+            _contextBD = contextDB;
+        }
+
+        public void GuardarParticipante(Usuarios usuarios)
+        {
+            _contextBD.Usuarios.Add(usuarios);
+            _contextBD.SaveChanges();
+        }
+
+        public Usuarios[] ObtenerTodosLosParticipantes()
+        {
+            return _contextBD.Usuarios.ToArray();
+        }
+    }
+}
