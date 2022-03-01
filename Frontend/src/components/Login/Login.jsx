93d411ba79e413183f3608fe0122089078e1c47c
luiggylml <luiggylml@gmail.com>
2022-03-01 17:17:18 -0500

    feat register complete

--- Frontend/src/components/Login/Login.jsx
+++ Frontend/src/components/Login/Login.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/logo.png";
 import { StyledLogin } from "./styles";
 import { AiFillUnlock } from "react-icons/ai";
 import { Link } from "react-router-dom";
 
 export const Login = () => {
+  const [inputLogin, setInputLogin] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInputLogin({
+      ...inputLogin,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputLogin);
+    setInputLogin({
+      email: "",
+      password: "",
+    });
+  };
+
   return (
     <StyledLogin>
       <div className="header">
@@ -13,12 +34,22 @@ export const Login = () => {
         <span></span>
         <p>Enter your e-mail address and your password</p>
       </div>
-      <form action="">
-        <input className="input-user" type="text" placeholder="User Name" />
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          className="input-user"
+          type="text"
+          placeholder="User Name"
+          name="email"
+          value={inputLogin.email}
+          onChange={handleChange}
+        />
         <input
           className="input-pass"
           type="password"
           placeholder="Type Password"
+          name="password"
+          value={inputLogin.password}
+          onChange={handleChange}
         />
         <div className="login">
           <input className="input-submit" type="submit" value="Login" />
--- Frontend/src/components/Register/Register.jsx
+++ Frontend/src/components/Register/Register.jsx
@@ -2,8 +2,49 @@ import React from "react";
 import { StyledRegister } from "./styles";
 import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export const Register = () => {
+  const [inputRegister, setInputRegister] = React.useState({
+    nombre: "",
+    apellidos: "",
+    email: "",
+    fechanacimiento: "",
+    password: "",
+  });
+
+  const users = async () => {
+    const response = await axios.get("https://localhost:44322/api/Usuarios");
+    console.log(response.data);
+  };
+
+  const register = async () => {
+    const response = await axios.post(
+      "https://localhost:44322/api/Usuarios",
+      inputRegister
+    );
+  };
+
+  const handleChange = (e) => {
+    setInputRegister({
+      ...inputRegister,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /*     console.log(inputRegister); */
+    setInputRegister({
+      nombre: "",
+      apellidos: "",
+      email: "",
+      fechanacimiento: "",
+      password: "",
+    });
+    register();
+  };
+  users();
   return (
     <StyledRegister>
       <div className="header">
@@ -12,12 +53,42 @@ export const Register = () => {
         <span></span>
         <p>Enter your personal detail below:</p>
       </div>
-      <form action="">
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Last Name" />
-        <input type="text" placeholder="Email Address" />
-        <input type="date" placeholder="date" />
-        <input type="password" placeholder="Password" />
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          name="nombre"
+          onChange={handleChange}
+          value={inputRegister.nombre}
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          name="apellidos"
+          onChange={handleChange}
+          value={inputRegister.apellidos}
+        />
+        <input
+          type="text"
+          placeholder="Email Address"
+          name="email"
+          value={inputRegister.email}
+          onChange={handleChange}
+        />
+        <input
+          type="date"
+          placeholder="date"
+          name="fechanacimiento"
+          value={inputRegister.fechanacimiento}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={inputRegister.password}
+          onChange={handleChange}
+        />
         <div className="terms">
           <input type="checkbox" className="checkbox" />
           <label htmlFor="">
